023543149167cc3b7461f131408b9a397be76d44
Quack <quackduck711@gmail.com>
2022-11-12 01:37:28 +0100

    Started working on game screen

--- test.py
+++ test.py
@@ -17,10 +17,10 @@ class WindowManager(ScreenManager):
 
 
 class MainMenu(Screen):
-    timerText = StringProperty('5')
+    startTime = 5
+    timerText = StringProperty(str(startTime))
     running = False
     expired = False
-    startTime = 5
     timeLeft = startTime
 
     # Skifter running til modsatte boolske vaerdi
@@ -42,14 +42,40 @@ class MainMenu(Screen):
 
     def timeControl(self, amount):
         self.timeLeft += amount
+        if self.timeLeft < 0:
+            self.timeLeft += 1
         self.update(1.0 / 30.0)
+        return self.timeLeft
+
+    # TODO: Figure out how to transfer timeLeft from this class to another
+    # TODO: Seems like it takes the "default" value instead of the changed..
 
 
 class PlayingScreen(Screen):
-    pass
+    # TODO: Game logic goes here
+    gameTimerText = MainMenu.timerText
+    gameTimer = MainMenu.timeLeft
+    running = False
+    expired = False
+
+    def update(self, dt):
+        if self.running:
+            self.gameTimer = self.gameTimer - dt
+        if self.gameTimer <= 0 and self.running:
+            print("UH OH YOU DONE!") # TODO: Change to end screen
+            self.running = False
+            self.expired = True
+
+        self.gameTimerText = str(math.ceil(self.gameTimer))
+
+    def gameLoop(self):
+        self.running = True
+        Clock.schedule_interval(self.update, 1.0/30.0)
+        return self
 
 
 class EndScreen(Screen):
+    # TODO: End screen logic goes here
     pass
 
 
